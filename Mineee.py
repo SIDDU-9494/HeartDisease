d6c91e2690ab536b166b45c74f32292a8c0605ab
SIDDU-9494 <110046020+SIDDU-9494@users.noreply.github.com>
2023-01-06 21:24:19 +0530

    Delete Mineee.py

--- Mineee.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import streamlit as st
-pickle_in=open('Predicts.pkl','rb')
-model=pickle.load(pickle_in)
-ef=st.number_input('Enter heartbeat')
-if st.button('Health'):
-  rs=model.predict([[ef]]).squeeze()
-  st.success(f'Health is {rs}')
